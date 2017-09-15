ed43848255a7282d9941364f5f34965c2885901a
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-09-15 20:33:44 +0530

    added implementation for compose

--- lib/fun.ex
+++ lib/fun.ex
@@ -1,18 +1,35 @@
 defmodule Fun do
   @moduledoc """
-  Documentation for Fun.
+  A collection of functions to allow your code to be more functional
   """
 
   @doc """
-  Hello world.
+  compose
 
   ## Examples
 
-      iex> Fun.hello
-      :world
+      iex> prefix = Fun.compose([&String.upcase/1, fn x -> String.slice(x, 0..2) end])
+      iex> prefix.("danny is awesome")
+      "DAN"
 
   """
-  def hello do
-    :world
+  def compose(functions) when is_list(functions) do
+    fn arg -> compose_apply(functions, arg) end
   end
+
+  def compose_apply([], arg), do: arg
+  def compose_apply([fun | functions], arg) do
+    compose_apply functions, apply(fun, [arg])
+  end
+
+  @doc """
+  The identity function returns the input arguments unchanged
+
+  ## Examples
+
+      iex> Fun.id(:awesome)
+      :awesome
+
+  """
+  def id(x), do: x
 end
--- test/fun_test.exs
+++ test/fun_test.exs
@@ -2,7 +2,15 @@ defmodule FunTest do
   use ExUnit.Case
   doctest Fun
 
-  test "greets the world" do
-    assert Fun.hello() == :world
+  describe "map_ok" do
+  end
+
+  describe "compose" do
+
+    test "allows composing of 2 functions" do
+      ten_times_length = Fun.compose([&String.length/1, fn x -> x * 10 end])
+      assert ten_times_length.("danny") == 50
+    end
+
   end
 end
